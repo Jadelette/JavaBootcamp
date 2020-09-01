6a51d2859716a4fb22047d32b38be20543f81774
Jade Corcoran <jade.corcoran@thoughtworks.com>
2020-09-01 16:33:37 +0100

    Kata2: All tests passing and can run from CLI with args

--- 02_Calculator/src/main/java/bootcamp/app/CalculatorApp.java
+++ 02_Calculator/src/main/java/bootcamp/app/CalculatorApp.java
@@ -1,11 +1,24 @@
 package bootcamp.app;
 
+import bootcamp.calculator.Calculator;
+import bootcamp.data.Params;
+import java.math.BigDecimal;
+
 /**
  * TODO: The application should be able to handle 3 arguments (first number, second number, operation).
  * The application should write the results to the standard output.
  */
 public class CalculatorApp {
     public static void main(final String[] args) {
-        throw new RuntimeException("Not yet implemented");
+        BigDecimal x = new BigDecimal(args[0]);
+        BigDecimal y = new BigDecimal(args[1]);
+        String operator = args[2];
+
+        Params params = new Params(x,y,operator);
+        Calculator calculator = new Calculator();
+
+        BigDecimal result = calculator.calculate(params);
+
+        System.out.println(result);
     }
 }
--- 02_Calculator/src/main/java/bootcamp/calculator/Calculator.java
+++ 02_Calculator/src/main/java/bootcamp/calculator/Calculator.java
@@ -3,9 +3,34 @@ package bootcamp.calculator;
 import bootcamp.data.Params;
 
 import java.math.BigDecimal;
+import java.math.*;
 
 public class Calculator {
     public BigDecimal calculate(final Params params) {
-        return null;
+        BigDecimal result;
+        MathContext m = new MathContext(2);
+
+        switch (params.getOperator()) {
+            case "+": {
+                result = params.getX().add(params.getY());
+            }
+            break;
+            case "-": {
+                result = params.getX().subtract(params.getY());
+            }
+            break;
+            case "x": {
+                result = params.getX().multiply(params.getY());
+            }
+            break;
+            case "/": {
+                result = params.getX().divide(params.getY());
+            }
+            break;
+            default: {
+                throw new IllegalArgumentException("operator not recognised");
+            }
+        }
+        return result.round(m).stripTrailingZeros();
     }
 }
